3088cc347b12ea526f8dc772bbf64cbc09341e5d
Brendan Poon <bpoon24@students.polytechnic.org>
2024-01-12 14:43:43 -0800

    Trying to create an error

--- QuadSolver.java
+++ QuadSolver.java
@@ -1,6 +1,6 @@
 public class QuadSolver {
     public static void main(String[] args) {
-        System.out.println("Hello.  I am the Quadratic Solver, and I will destroy you.");
+        System.out.println("I am the Quadratic Solver, and I will destroy you.");
         System.out.println("The quadratic formula is -b +- sqrt(b^2 - 4c)/2a");
     }
 }
