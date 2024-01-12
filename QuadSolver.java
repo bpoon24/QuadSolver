632ddc6441fb47f89c1bff8bb100fa1357f2a694
bpoon24 <156132028+bpoon24@users.noreply.github.com>
2024-01-12 14:42:56 -0800

    Update QuadSolver.java

--- QuadSolver.java
+++ QuadSolver.java
@@ -1,5 +1,5 @@
 public class QuadSolver {
     public static void main(String[] args) {
-        System.out.println("Welcome to the QuadSolver!");
+        System.out.println("Welcome to the QuadSolver! I will destroy you.");
     }
 }
